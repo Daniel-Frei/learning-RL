ae317c434d12ab1704204c6a280870934c20c4e9
Daniel Frei <itsdanielfrei@gmail.com>
2026-02-05 15:53:02 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Button, TextBox
+
+
+def main():
+    print("Hello from learning-rl!")
+
+
+if __name__ == "__main__":
+    main()
